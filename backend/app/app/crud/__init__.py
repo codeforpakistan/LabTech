b35002a356048ae8246c57b2b4b64fa8e9a6982c
Hassan Mehmood <hasanmehmood123@yahoo.com>
2020-12-11 15:36:33 +0500

    update endpoints

--- backend/app/app/crud/__init__.py
+++ backend/app/app/crud/__init__.py
@@ -4,6 +4,7 @@ from .crud_hospital import hospital
 from .crud_department import department
 from .crud_survey import survey
 from .crud_feeback import feedback
+from .crud_submission import submission
 
 
 # For a new basic set of CRUD operations you could just do
---
+++ backend/app/app/crud/crud_submission.py
@@ -0,0 +1,34 @@
+from typing import List
+
+from fastapi.encoders import jsonable_encoder
+from sqlalchemy.orm import Session
+
+from app.crud.base import CRUDBase
+from app.models.submission import Submission
+from app.schemas.submission import SubmissionCreate, SubmissionUpdate
+
+
+class CRUDSubmission(CRUDBase[Submission, SubmissionCreate, SubmissionUpdate]):
+    def create_with_owner(
+        self, db: Session, *, obj_in: SubmissionCreate, owner_id: int
+    ) -> Submission:
+        obj_in_data = jsonable_encoder(obj_in)
+        db_obj = self.model(**obj_in_data, owner_id=owner_id)
+        db.add(db_obj)
+        db.commit()
+        db.refresh(db_obj)
+        return db_obj
+
+    def get_multi_by_owner(
+        self, db: Session, *, owner_id: int, skip: int = 0, limit: int = 100
+    ) -> List[Submission]:
+        return (
+            db.query(self.model)
+            .filter(Item.owner_id == owner_id)
+            .offset(skip)
+            .limit(limit)
+            .all()
+        )
+
+
+submission = CRUDSubmission(Submission)
