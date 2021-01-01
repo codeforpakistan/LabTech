4de390cbd7d3fccc720f51ab22ad373ecfa0f837
sohail0992 <msohail.se@gmail.com>
2021-01-01 14:42:12 +0500

    departments adding logic inside hospital

---
+++ frontend/src/views/main/admin/hospital/departments/allDepartments.vue
@@ -0,0 +1,97 @@
+import { Component, Vue } from 'vue-property-decorator';
+<template>
+  <div>
+    <v-toolbar light>
+      <v-toolbar-title>
+        Manage Departments
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn color="primary" to="/main/admin/hospital/1/create">Create Department</v-btn>
+    </v-toolbar>
+    <v-data-table :headers="headers" :items="hospitals">
+      <template slot="items" slot-scope="props">
+        <td>{{ props.item.name }}</td>
+        <td>{{ props.item.address }}</td>
+        <td>{{ props.item.created_date }}</td>
+        <td>{{ props.item.lat }}</td>
+        <td>{{ props.item.lng }}</td>
+        <td class="justify-center layout px-0" colspan="2">
+          <v-tooltip top>
+            <span>View / Edit Departments</span>
+            <v-btn slot="activator" flat :to="{name: 'main-admin-hospitals-departments-survey', params: {id: props.item.id}}">
+              <v-icon>home</v-icon>
+            </v-btn>
+          </v-tooltip>
+          <v-tooltip top>
+            <span>Edit</span>
+            <v-btn slot="activator" flat :to="{name: 'main-admin-users-edit', params: {id: props.item.id}}">
+              <v-icon>edit</v-icon>
+            </v-btn>
+          </v-tooltip>
+        </td>
+      </template>
+    </v-data-table>
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import { Store } from 'vuex';
+import { readAdminHospital } from '@/store/admin/getters';
+import { dispatchGetHospitalDepartments } from '@/store/admin/actions';
+import { IHospital } from '@/interfaces';
+
+
+@Component
+export default class AdminHospitals extends Vue {
+  [x: string]: any;
+  public id: any;
+  public headers = [
+    {
+      text: 'Name',
+      sortable: true,
+      value: 'name',
+      align: 'left',
+    },
+    {
+      text: 'Address',
+      sortable: true,
+      value: 'address',
+      align: 'left',
+    },
+    {
+      text: 'Created Date',
+      sortable: true,
+      value: 'create_date',
+      align: 'left',
+    },
+    {
+      text: 'Lat',
+      sortable: true,
+      value: 'lat',
+      align: 'left',
+    },
+    {
+      text: 'lng',
+      sortable: true,
+      value: 'lng',
+      align: 'left',
+    },
+    {
+      text: 'Actions',
+      value: 'id',
+      colspan: 2,
+      align: 'center',
+    },
+  ];
+
+  public async mounted() {
+    this.id = this.$router.currentRoute.params.id;
+    await dispatchGetHospitalDepartments(this.$store, this.id);
+  }
+
+  get hospitals() {
+    return readAdminHospital(this.$store);
+  }
+}
+</script>
