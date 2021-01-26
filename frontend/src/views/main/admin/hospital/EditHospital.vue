90d9a161b21f4dc39d40825500f7ffa5b669a87b
sohail0992 <msohail.se@gmail.com>
2021-01-26 13:54:08 +0500

    update workflow for hospital and hospital department

---
+++ frontend/src/views/main/admin/hospital/EditHospital.vue
@@ -0,0 +1,94 @@
+<template>
+  <v-container fluid>
+    <v-card class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline primary--text">Edit Hospital</div>
+      </v-card-title>
+      <v-card-text>
+        <template>
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <v-text-field label="Name" v-model="name" required></v-text-field>
+            <v-text-field label="Address" type="text" v-model="address" required></v-text-field>
+            <v-text-field label="latitude" type="text" v-model="lat" required></v-text-field>
+            <v-text-field label="longitude" type="text" v-model="lng" required></v-text-field>
+          </v-form>
+        </template>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="cancel">Cancel</v-btn>
+        <v-btn @click="reset">Reset</v-btn>
+        <v-btn @click="submit" :disabled="!valid">
+          Update
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import { IHospitalUpdate } from '@/interfaces';
+  import { dispatchGetHospitals, dispatchUpdateHospital } from '@/store/admin/actions';
+  import { readAdminOneHospital } from '@/store/admin/getters';
+
+  @Component
+  export default class EditUser extends Vue {
+    public valid = false;
+    public name: string = '';
+    public id: number = -1;
+    public address: string = '';
+    public lat: string = '';
+    public lng: string = '';
+
+    public async mounted() {
+      await dispatchGetHospitals(this.$store);
+      this.id = parseInt(this.$router.currentRoute.params.id, 10);
+      this.setData(this.hospital);
+      this.reset();
+    }
+
+    public reset() {
+      this.name = '';
+      this.address = '';
+      this.lat = '';
+      this.lng = '';
+      this.$validator.reset();
+      if (this.hospital) {
+        this.setData(this.hospital);
+      }
+    }
+
+    public setData(hospital) {
+      this.name = hospital?.name;
+      this.id = hospital?.id;
+      this.address = hospital?.address;
+      this.lat = hospital?.lat;
+      this.lng = hospital?.lng;
+    }
+
+    public cancel() {
+      this.$router.back();
+    }
+
+    public async submit() {
+      if (await this.$validator.validateAll()) {
+        const updatedHopital: IHospitalUpdate = {
+          id: this.id,
+          name: this.name,
+          address: this.address,
+          lat: this.lat,
+          lng: this.lng,
+          create_date: new Date(),
+          owner_id: 1,
+        };
+        await dispatchUpdateHospital(this.$store, {id: this.id, hospital: updatedHopital});
+        this.$router.push('/main/admin/hospital');
+      }
+    }
+
+    get hospital() {
+      return readAdminOneHospital(this.$store)(+this.id);
+    }
+  }
+</script>
---
+++ frontend/src/views/main/admin/hospital/departments/EditDepartment.vue
@@ -0,0 +1,78 @@
+<template>
+  <v-container fluid>
+    <v-card class="ma-3 pa-3">
+      <v-card-title primary-title>
+        <div class="headline primary--text">Create Department</div>
+      </v-card-title>
+      <v-card-text>
+        <template>
+          <v-form v-model="valid" ref="form" lazy-validation>
+            <v-text-field label="Name" v-model="name" required></v-text-field>
+          </v-form>
+        </template>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn @click="cancel">Cancel</v-btn>
+        <v-btn @click="reset">Reset</v-btn>
+        <v-btn @click="submit" :disabled="!valid">
+          Update
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-container>
+</template>
+
+<script lang="ts">
+  import { Component, Vue } from 'vue-property-decorator';
+  import { IDepartmentUpdate } from '@/interfaces';
+  import { dispatchGetHospitalDepartments, dispatchUpdateDepartment } from '@/store/admin/actions';
+  import { readAdminOneDepartment } from '@/store/admin/getters';
+
+  @Component
+  export default class EditDepartment extends Vue {
+    public valid = false;
+    public name: string = '';
+    private id: number = -1;
+    private departmentId: number = -1;
+
+    public async mounted() {
+      this.id = parseInt(this.$router.currentRoute.params.id, 10);
+      this.departmentId = parseInt(this.$router.currentRoute.params.departmentId, 10);
+      await dispatchGetHospitalDepartments(this.$store, this.id);
+      this.setData(this.department);
+      this.reset();
+    }
+
+    public reset() {
+      this.name = '';
+      this.$validator.reset();
+      if (this.department) {
+        this.setData(this.department);
+      }
+    }
+
+    public setData(department) {
+      this.name = department?.name;
+    }
+
+    public cancel() {
+      this.$router.back();
+    }
+
+    public async submit() {
+      if (await this.$validator.validateAll()) {
+        const updatedDepartment: IDepartmentUpdate = {
+          id: this.departmentId,
+          name: this.name,
+        };
+        await dispatchUpdateDepartment(this.$store, {id: this.departmentId, department: updatedDepartment});
+        this.$router.push('/main/admin/hospital/' + this.id);
+      }
+    }
+
+    get department() {
+      return readAdminOneDepartment(this.$store)(+this.departmentId);
+    }
+  }
+</script>
