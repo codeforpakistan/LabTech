d556884ec0b36db5bfe0b63182af9f721a15e12e
sohail0992 <msohail.se@gmail.com>
2021-03-08 09:25:09 +0500

    code formatting

--- frontend/src/views/main/admin/CreateUser.vue
+++ frontend/src/views/main/admin/CreateUser.vue
@@ -1,81 +1,81 @@
 <template>
   <v-container fluid>
-    <v-card class='ma-3 pa-3'>
+    <v-card class="ma-3 pa-3">
       <v-card-title primary-title>
-        <div class='headline primary--text'>Create User</div>
+        <div class="headline primary--text">Create User</div>
       </v-card-title>
       <v-card-text>
         <template>
-          <v-form v-model='valid' ref='form' lazy-validation>
+          <v-form v-model="valid" ref="form" lazy-validation>
             <v-text-field
-              label='Full Name'
-              v-model='fullName'
+              label="Full Name"
+              v-model="fullName"
               required
             ></v-text-field>
             <v-text-field
-              label='E-mail'
-              type='email'
-              v-model='email'
-              v-validate=''required|email''
-              data-vv-name='email'
-              :error-messages='errors.collect('email')'
+              label="E-mail"
+              type="email"
+              v-model="email"
+              v-validate="'required|email'"
+              data-vv-name="email"
+              :error-messages="errors.collect('email')"
               required
             ></v-text-field>
-            <div class='subheading secondary--text text--lighten-2'>
+            <div class="subheading secondary--text text--lighten-2">
               User is superuser
-              <span v-if='isSuperuser'>(currently is a superuser)</span
+              <span v-if="isSuperuser">(currently is a superuser)</span
               ><span v-else>(currently is not a superuser)</span>
             </div>
-            <v-checkbox label='Is Superuser' v-model='isSuperuser'></v-checkbox>
-            <div class='subheading secondary--text text--lighten-2'>
-              User is active <span v-if='isActive'>(currently active)</span
+            <v-checkbox label="Is Superuser" v-model="isSuperuser"></v-checkbox>
+            <div class="subheading secondary--text text--lighten-2">
+              User is active <span v-if="isActive">(currently active)</span
               ><span v-else>(currently not active)</span>
             </div>
-            <v-checkbox label='Is Active' v-model='isActive'></v-checkbox>
+            <v-checkbox label="Is Active" v-model="isActive"></v-checkbox>
             <v-layout align-center>
               <v-flex>
                 <v-text-field
-                  type='password'
-                  ref='password'
-                  label='Set Password'
-                  data-vv-name='password'
-                  data-vv-delay='100'
-                  v-validate='{ required: true }'
-                  v-model='password1'
-                  :error-messages='errors.first('password')'
+                  type="password"
+                  ref="password"
+                  label="Set Password"
+                  data-vv-name="password"
+                  data-vv-delay="100"
+                  v-validate="{ required: true }"
+                  v-model="password1"
+                  :error-messages="errors.first('password')"
                 >
                 </v-text-field>
                 <v-text-field
-                  type='password'
-                  label='Confirm Password'
-                  data-vv-name='password_confirmation'
-                  data-vv-delay='100'
-                  data-vv-as='password'
-                  v-validate='{ required: true, confirmed: 'password' }'
-                  v-model='password2'
-                  :error-messages='errors.first('password_confirmation')'
+                  type="password"
+                  label="Confirm Password"
+                  data-vv-name="password_confirmation"
+                  data-vv-delay="100"
+                  data-vv-as="password"
+                  v-validate="{ required: true, confirmed: 'password' }"
+                  v-model="password2"
+                  :error-messages="errors.first('password_confirmation')"
                 >
                 </v-text-field>
               </v-flex>
             </v-layout>
-            <div class='mt-3'>
+            <div class="mt-3">
               <v-label>Allowed Hospitals</v-label>
               <v-layout row wrap>
-                <v-flex v-for='(category, index) in types' :key='types[index].text' xs6>
+                <v-flex v-for="(category, index) in types" :key="types[index].text" xs6>
                   <v-checkbox
                     light
-                    :label='category.text'
-                    v-model='category.selected'
+                    :label="category.text"
+                    v-model="category.selected"
                   >
                   </v-checkbox>
                 </v-flex>
               </v-layout>
               <v-select
-                v-model='selectedHospitals'
-                :items='hospitals'
-                item-text='name'
-                item-value='id'
-                label='Select Allowed Hospitals'
+                v-model="selectedHospitals"
+                :items="hospitals"
+                item-text="name"
+                item-value="id"
+                label="Select Allowed Hospitals"
                 persistent-hint
                 return-object
                 single-line
@@ -88,23 +88,23 @@
       </v-card-text>
       <v-card-actions>
         <v-spacer></v-spacer>
-        <v-btn @click='cancel'>Cancel</v-btn>
-        <v-btn @click='reset'>Reset</v-btn>
-        <v-btn @click='submit' :disabled='!valid'> Save </v-btn>
+        <v-btn @click="cancel">Cancel</v-btn>
+        <v-btn @click="reset">Reset</v-btn>
+        <v-btn @click="submit" :disabled="!valid"> Save </v-btn>
       </v-card-actions>
     </v-card>
   </v-container>
 </template>
 
-<script lang='ts'>
-import { Component, Vue } from 'vue-property-decorator';
-import { IHospital, IUserProfileCreate } from '@/interfaces';
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import { IHospital, IUserProfileCreate } from "@/interfaces";
 import {
   dispatchGetUsers,
   dispatchCreateUser,
   dispatchGetHospitals,
-} from '@/store/admin/actions';
-import { readAdminHospital } from '@/store/admin/getters';
+} from "@/store/admin/actions";
+import { readAdminHospital } from "@/store/admin/getters";
 
 @Component
 export default class CreateUser extends Vue {
@@ -184,7 +184,7 @@ export default class CreateUser extends Vue {
       updatedProfile.is_superuser = this.isSuperuser;
       updatedProfile.password = this.password1;
       await dispatchCreateUser(this.$store, updatedProfile);
-      this.$router.push('/main/admin/users');
+      this.$router.push("/main/admin/users");
     }
   }
 }
