26868c7f40a6702b1207718affb1a930ce4231c0
sohail0992 <msohail.se@gmail.com>
2021-07-09 15:28:38 +0500

    absolute path

--- backend/app/app/api/api_v1/endpoints/utils.py
+++ backend/app/app/api/api_v1/endpoints/utils.py
@@ -13,9 +13,6 @@ from app.core.celery_app import celery_app
 from app.utils import send_test_email
 
 router = APIRouter()
-import os 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-
 
 @router.post("/test-celery/", response_model=schemas.Msg, status_code=201)
 def test_celery(
@@ -51,12 +48,11 @@ def save_upload_file(upload_file: UploadFile, destination: Path) -> None:
 
 @router.post("/uploadimage/")
 def create_upload_file(file: UploadFile = File(...)):
-    print(dir_path)
-    filepath = Path(dir_path + '/images') / file.filename
+    filepath = Path('../../../../images') / file.filename
     save_upload_file(file, filepath)
     return {"filename": file.filename}
 
 
 @router.get("/image/{image_name}")
 def get_image(image_name: str = None):
-    return FileResponse(f"${dir_path}/images/{image_name}")
+    return FileResponse(f"../../../../images/{image_name}")
