6efd50e3849c60847252eb5c9741921e336ed248
Hassan Mehmood <hasanmehmood123@yahoo.com>
2021-07-26 17:04:42 +0500

    fix issue

--- backend/app/app/api/api_v1/endpoints/submissions.py
+++ backend/app/app/api/api_v1/endpoints/submissions.py
@@ -242,10 +242,7 @@ def get_submissions_by_lab(
             'answers': submission.answers,
             'submission_id': submission.id
         })
-
-    print(submissions_list)
-
-
+    
     submissions_df = pd.DataFrame(submissions_list)
     labnames = list(submissions_df.name.unique())
     submissions_by_lab = []
@@ -263,7 +260,7 @@ def get_submissions_by_lab(
             ]
             submissions_by_lab.append({
                 'name': labname,
-                '_id': 0,
+                '_id': int(submissions_df_by_labname_by_no._id.iloc[-1]),
                 'submission_no': submission_no,
                 'submissions': submissions_df_by_labname_by_no.to_dict(orient='records')
             })
