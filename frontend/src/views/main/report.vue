ecc177d771a95b2adcbd3e3319659bf07bd48154
saqibhussain44 <saqibhussain990@gmail.com>
2021-08-16 13:33:00 +0500

    all submissions option

--- frontend/src/views/main/report.vue
+++ frontend/src/views/main/report.vue
@@ -80,11 +80,11 @@
           <v-select :items="labOpts" v-model="selectedLab" @change="setLabID" label="Lab Name" dense></v-select>
         </v-flex>
         <v-flex md6 style="padding-left: 10px" v-if="loadedSubmissionNumber && labSubmissionNumbers">
-          <v-select :items="labSubmissionNumbers.submission_nos" @change="fetchAccumulative" v-model="selectedSubmission"  filled label="Submission Number" dense></v-select>
+          <v-select :items="submissionOpts" @change="fetchAccumulative" v-model="selectedSubmission"  filled label="Submission Number" dense></v-select>
         </v-flex>
       </v-layout>
       </v-container>
-      <div  v-if="loadedSubmissionNumber && byLabReportAccumulative">
+      <div v-if="loadedSubmissionNumber && byLabReportAccumulative">
         <div v-for="item in byLabReportAccumulative" :key="item.name">
           <div class="mt-2" style="display: flex; justify-content: space-between; width: 340px;">
             <div><b>{{item.name}}</b></div>
@@ -92,7 +92,7 @@
           </div>
           <div style="display: flex; justify-content: space-between;">
             <vue-excel-editor ref="editor" width="400px" :readonly="true" v-model="item.data.indicators" :localized-label="myLabels"></vue-excel-editor>
-            <highcharts style="margin-top: -20px;" :options="getHighchartData(item)"></highcharts>
+            <highcharts v-if="item.data.indicators && item.data.indicators.length > 0" style="margin-top: -20px;" :options="getHighchartData(item)"></highcharts>
           </div>
         </div>
       </div>
@@ -102,12 +102,9 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import { Store } from 'vuex';
 import Highcharts from 'highcharts';
-import drilldown from 'highcharts/modules/drilldown';
 import VueExcelEditor from 'vue-excel-editor';
 Vue.use(VueExcelEditor);
-drilldown( Highcharts );
 import { readOverAllStatistics, readHospitalsStatistics, readlabSubmissionNumbers,
   readAdminHospital, readHospitalDepartments, readByLabReport, readbyLabReportAccumulative } from '@/store/admin/getters';
 import { dispatchGetOverAllStatistics, dispatchGetHospitalStatistics, dispatchGetByLabReport,
@@ -253,35 +250,25 @@ export default class Reporting extends Vue {
     return readlabSubmissionNumbers(this.$store);
   }
 
-  private calculateTotalSubmissions(fallback) {
-    this.totalSubmissions = this.hospitalStatistics && this.hospitalStatistics[0]
-    && this.hospitalStatistics[0].total_submissions
-    ? this.hospitalStatistics[0].total_submissions
-    : fallback && this.overAllStatistics && this.overAllStatistics[0]
-      && this.overAllStatistics[0].total_submissions
-      ? this.overAllStatistics && this.overAllStatistics[0].total_submissions
-      : 0;
-  }
-
   get labOpts() {
     return this.hospitals.map((x: any) => x.name);
   }
 
+  get submissionOpts() {
+    return ['All', ...this.labSubmissionNumbers.submission_nos];
+  }
+
   private async mounted() {
     await dispatchGetHospitals(this.$store);
-    this.consturctOverAllStatistics();
     this.selectedLab = this.hospitals[0].name;
     this.setLabID();
   }
 
-  private async consturctOverAllStatistics() {
-    await dispatchGetOverAllStatistics(this.$store);
-    this.calculateTotalSubmissions(true);
-    this.constructSurveyChart(this.overAllStatistics);
-  }
-
   private async fetchAccumulative() {
-    const query = `?apply_filter=1&lab_id=${this.labID}&submission_no=${this.selectedSubmission}`;
+    let query = `?apply_filter=1&lab_id=${this.labID}&submission_no=${this.selectedSubmission}`;
+    if (this.selectedSubmission === 'All') {
+      query = `?apply_filter=1&lab_id=${this.labID}&submission_no=0`;
+    }
     await dispatchGetByLabReportAccumulative(this.$store, query);
   }
 
@@ -295,7 +282,7 @@ export default class Reporting extends Vue {
     await dispatchGetByLabReport(this.$store);
     await dispatchGetLabSubmissions(this.$store, this.labID);
     this.configureSubmissionListAndSelectedItem();
-    const query = `?apply_filter=1&lab_id=${this.labID}&submission_no=${this.selectedSubmission}`;
+    const query = `?apply_filter=0&lab_id=${this.labID}&submission_no=${this.selectedSubmission}`;
     await dispatchGetByLabReportAccumulative(this.$store, query);
     this.loadedSubmissionNumber = true;
   }
@@ -304,157 +291,6 @@ export default class Reporting extends Vue {
     if (this.labSubmissionNumbers && this.labSubmissionNumbers.submission_nos
       && this.labSubmissionNumbers.submission_nos.length > 0) {
       this.selectedSubmission =  this.labSubmissionNumbers.submission_nos[0];
-    }
-  }
-
-  private async constructSelectedHospitalStatistics(hospital) {
-    this.totalSubmissions = 0;
-    this.selectedDepartment = {
-      id: '',
-      name: '',
-    };
-    await dispatchGetHospitalDepartments(this.$store, hospital.id);
-    await dispatchGetHospitalStatistics(this.$store, {
-      hospitalId: this.select.id,
-      departmentId: 0,
-    });
-    this.calculateTotalSubmissions(false);
-    this.constructSurveyChart(this.hospitalStatistics);
-  }
-
-  private reset() {
-    this.selectedDepartment = {
-      id: '',
-      name: '',
-    };
-    this.select = {
-      id: '',
-      name: '',
-    };
-  }
-
-  private async changeDepartment(value) {
-    if (!value && !this.select.id) {
-      this.reset();
-      this.consturctOverAllStatistics();
-      return;
-    } else if (this.select.id && !value) {
-      this.constructSelectedHospitalStatistics(this.select);
-    } else {
-      this.totalSubmissions = 0;
-      await dispatchGetHospitalStatistics(this.$store, {
-        hospitalId: this.select.id,
-        departmentId: this.selectedDepartment.id,
-      });
-      this.calculateTotalSubmissions(false);
-      this.constructSurveyChart(this.hospitalStatistics);
-    }
-
-  }
-
-  private async changeValue(value) {
-    if (!value) {
-      this.reset();
-      this.consturctOverAllStatistics();
-      return;
-    } else {
-      this.constructSelectedHospitalStatistics(value);
-    }
-  }
-
-  private constructSurveyChart(hospitalStatistics) {
-    const vm = this;
-    const pdata: any = [];
-    const ndata: any = [];
-    if (hospitalStatistics && hospitalStatistics[0] && hospitalStatistics[0].by_question) {
-        hospitalStatistics[0].by_question.forEach((each: any) => {
-          if (each.question) {
-            pdata.push([each.question, each.answer_true_perc, each.color, each.weightage]);
-            ndata.push([each.question, -each.answer_false_perc, each.color, each.weightage]);
-          }
-        });
-        Highcharts.chart({
-          chart: {
-            renderTo: 'container',
-            type: 'column',
-            inverted: true,
-          },
-          plotOptions: {
-            series: {
-              stacking: 'normal',
-              dataLabels: {
-                style: {
-                  fontSize: '9px',
-                },
-                format: '{y}%',
-              },
-            },
-          },
-          credits: {
-            text: `<b>Total Survey Submissions  ${this.totalSubmissions}</b>`,
-          },
-          yAxis: {
-            labels: {
-              format: '{value}%',
-              enabled: true,
-            },
-            stackLabels: {
-              enabled: true,
-              formatter() {
-                return Math.abs(this.total) + '%';
-              },
-            },
-            gridLineWidth: 0,
-            title: {
-              text: null,
-            },
-            floor: -210,
-          },
-          xAxis: {
-            labels: {
-              enabled: true,
-              style: {
-                fontSize: '13px',
-              },
-              formatter() {
-                const data = pdata.find((each) => each[0] === this.value);
-                return `<span style="color: ${data && data[2] ? data[2] : '#000000'}">${this.value.toString().toUpperCase()}</span>`;
-              },
-            },
-            lineWidth: 0,
-            tickLength: 0,
-            type: 'category',
-          },
-          exporting: {
-            enabled: false,
-          },
-          title: {
-            text: `Each Survey Question Response by <b>${this.selectedDepartment.name ? this.select.name + ' '
-            + this.selectedDepartment.name : this.select.name ? this.select.name : 'all hospitals'} ${this.selectedDepartment.name ? 'Department' : 'Departments'}</b>`,
-          },
-          tooltip: {
-            formatter() {
-              return '<span style="font-size: 10px">' + this.key + '</span><br/>' +
-                '<span style="color:' + this.color + '">\u25CF </span>' + this.series.name + ': <b>'
-                + Math.abs(this.y) + '%</b><br/>';
-            },
-          },
-          series: [{
-            type: 'column',
-            index: 0,
-            color: '#dd4532',
-            name: 'Strongly Distrust',
-            data: ndata,
-           }, {
-            type: 'column',
-            index: 1,
-            color: '#2eae94',
-            name: 'Strongly Trust',
-            data: pdata,
-          }],
-        });
-    } else {
-      // alert('error');
     }
   }
 
